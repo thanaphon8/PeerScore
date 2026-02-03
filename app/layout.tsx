af75a0534065dc20239c026b1021716bf1c0a7ac
Thanaphon <dthanaphon21@gmail.com>
2026-02-03 23:38:22 +0700

    remove_animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { LoadingBar } from './components/LoadingBar';
-import './styles/transitions.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <LoadingBar />
         {children}
       </body>
     </html>
